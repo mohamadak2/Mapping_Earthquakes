c814c06f6a7be9f193cc830a4f75f478a5ef7f85
mohamadak2 <47649575+mohamadak2@users.noreply.github.com>
2021-11-14 17:32:52 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,40 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+let baseMaps = {
+"Streets" :streets,
+"Satellite Streets": satelliteStreets
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid',{
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
+});
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/mohamadak2/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+d3.json(torontoHoods).then(function(data){
+console.log(data);
+L.geoJSON(data).addTo(map);
+});
